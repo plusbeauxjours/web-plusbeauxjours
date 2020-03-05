a4360cadfd5a2c10dbdfa39368d2efd5bd70bf8c
plusbeauxjours <plusbeauxjours@gmail.com>
2020-03-05 14:24:04 +0900

    setup

---
+++ src/Routes/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import Helmet from "react-helmet";
+
+const Container = styled.div``;
+
+const Text = styled.text``;
+
+const HomeHome: React.FunctionComponent<any> = () => (
+  <Container>
+    <Helmet>
+      <title>Plusbeauxjours</title>
+    </Helmet>
+    <Text>plusbeauxjours</Text>
+  </Container>
+);
+
+export default HomeHome;
