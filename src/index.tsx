44477163633488dadda83aae4b647e73a4232a9a
plusbeauxjours <plusbeauxjours@gmail.com>
2020-05-30 12:00:15 +0700

    reactGA

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactGA from "react-ga";
+
 import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactGA.initialize("UA-166957436-1", {
+  debug: false,
+  titleCase: false,
+  gaOptions: {
+    siteSpeedSampleRate: 100,
+  },
+});
+if (window.location.host === "https://www.plusbeauxjours.com") {
+  ReactGA.pageview(window.location.pathname);
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
